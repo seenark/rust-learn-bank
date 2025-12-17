29814ee462f4491e68c313b303c8dc8218edbe23
Atiwat Seenark <um_oom@hotmail.com>
2025-12-17 16:52:09 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,35 @@ struct Account {
     holder: String,
 }
 
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account {
+            id,
+            balance: 0,
+            holder,
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Bank {
     accounts: Vec<Account>,
 }
 
+impl Bank {
+    fn new() -> Self {
+        Bank { accounts: vec![] }
+    }
+}
+
+fn print_account(account: Account) {
+    println!("{:#?}", account);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let bank = Bank::new();
+
+    let other_bank = bank;
+
+    println!(":#?", bank);
 }
