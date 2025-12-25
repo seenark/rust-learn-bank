cd9a7929968b0f28de05057c717182d5690fa03d
Atiwat Seenark <um_oom@hotmail.com>
2025-12-25 10:47:50 +0700

    udate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::format;
+
 #[derive(Debug)]
 struct Account {
     id: u32,
@@ -13,6 +15,20 @@ impl Account {
             holder,
         }
     }
+
+    fn deposite(&mut self, amount: i32) -> i32 {
+        self.balance += amount;
+        self.balance
+    }
+
+    fn withdraw(&mut self, amount: i32) -> i32 {
+        self.balance -= amount;
+        self.balance
+    }
+
+    fn summary(&self) -> String {
+        format!("{} has a balance {}", self.holder, self.balance)
+    }
 }
 
 #[derive(Debug)]
@@ -24,16 +40,34 @@ impl Bank {
     fn new() -> Self {
         Bank { accounts: vec![] }
     }
-}
 
-fn print_account(account: Account) {
-    println!("{:#?}", account);
+    fn add_account(&mut self, account: Account) {
+        self.accounts.push(account);
+    }
+
+    fn total_balance(&self) -> i32 {
+        self.accounts.iter().map(|account| account.balance).sum()
+    }
+
+    fn summary(&self) -> Vec<String> {
+        self.accounts
+            .iter()
+            .map(|account| account.summary())
+            .collect::<Vec<String>>()
+    }
 }
 
 fn main() {
-    let bank = Bank::new();
+    let mut bank = Bank::new();
+    let mut account = Account::new(1, String::from("me"));
 
-    let other_bank = bank;
+    account.deposite(500);
+    account.withdraw(250);
 
-    println!(":#?", bank);
+    println!("account = {:#?}", account.summary());
+
+    bank.add_account(account);
+
+    println!("bank = {:#?}", bank.summary());
+    println!("total balance = {}", bank.total_balance());
 }
